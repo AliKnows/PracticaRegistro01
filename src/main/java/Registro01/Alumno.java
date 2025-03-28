8711020b0dfb66b90853b521ed889e1e422a6191
AliKnows <130936574+AliKnows@users.noreply.github.com>
2025-03-28 12:53:21 -0500

    UPDATE tabla de registros

--- src/main/java/Registro01/Alumno.java
+++ src/main/java/Registro01/Alumno.java
@@ -17,12 +17,14 @@ public class Alumno {
     String facultad;
     double promedio;
     int edad;
-       
+    
+    
+    //constructor por defecto (No tiene nada)
     public Alumno(){
         
     }
     
-    //constructor
+    //constructores
     
     public Alumno(String name, String carrera, String codigo, String semestre, String facultad,
             double promedio,int edad){
@@ -35,6 +37,69 @@ public class Alumno {
         this.promedio = promedio;
         this.edad = edad;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "name=" + name + ", carrera=" + carrera + ", codigo=" + codigo + ", semestre=" + semestre + ", facultad=" + facultad + ", promedio=" + promedio + ", edad=" + edad + '}';
+    }
+    
+    
     
     
 }
