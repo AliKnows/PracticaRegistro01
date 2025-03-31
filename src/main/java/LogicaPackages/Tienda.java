1163a22e6f9509729625e5a1b638ffad382474bc
AliKnows <130936574+AliKnows@users.noreply.github.com>
2025-03-31 09:53:51 -0500

    UPDATE Registro clase Tienda

---
+++ src/main/java/LogicaPackages/Tienda.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LogicaPackages;
+
+/**
+ *
+ * @author AIAR
+ */
+public class Tienda {
+    
+    private int codigo_tienda;
+    public String nombre;
+    public String direccion;
+    
+    public void Registrar(){
+        
+    }
+
+    public Tienda() {
+    }
+
+    public Tienda(int codigo_tienda, String nombre, String direccion) {
+        this.codigo_tienda = codigo_tienda;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
