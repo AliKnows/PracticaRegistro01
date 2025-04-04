bcecd00e8ebe78df53af8367db79c20edb098b17
AliKnows <130936574+AliKnows@users.noreply.github.com>
2025-04-04 12:40:39 -0500

    Create Docente class

--- src/main/java/Registro01/Docente.java
+++ src/main/java/Registro01/Docente.java
@@ -30,6 +30,47 @@ public class Docente {
         
         
     }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Double getSueldo() {
+        return Sueldo;
+    }
+
+    public void setSueldo(Double Sueldo) {
+        this.Sueldo = Sueldo;
+    }
+    
     
     
     
